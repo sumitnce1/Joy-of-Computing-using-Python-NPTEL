f704929d1644eefceb2ea800d7c0814ed31d414b
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-11-03 20:44:03 +0530

    Update 2nd_set.py

--- Evening slot 8pm Python answer/2nd_set.py
+++ Evening slot 8pm Python answer/2nd_set.py
@@ -6,3 +6,17 @@ for i in frnds:
     if(i%j>=max):
       max=i%j
 print(max,end="")
+
+
+
+
+
+/*  2nd method*/
+n=int(input())
+f=list(map(int,input().split(" ")))
+a=[]
+for i in range(n):
+    for j in range(n):
+        b=f[i]%f[j]
+        a.append(b)
+print(max(a),end="")
